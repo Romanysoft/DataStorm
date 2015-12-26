90bdce2a983394a24d26e8c72d5ad09f8b918c0e
iansun <lauer3912@gmail.com>
2015-12-26 01:16:50 -0800

    更新下载地址，中文版，同时还有google分析ID

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
         appName:"DataStorm",
         appID:"com.romanysoft.app.macos.DataStorm",
         documentTitle:"DataStorm By Romanysoft",
-        googleUA:"UA-43238484-7",
+        googleUA:"UA-43238484-5",
         gitHome:"https://github.com/Romanysoft/DataStorm",
         romanysoftHome:"http://www.romanysoft.com",
         copyright:"Copyright © 2016 Romanysoft LAB."
