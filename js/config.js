853b4b721e5793af64baacc7d842c6427de4f416
iansun <lauer3912@gmail.com>
2016-01-26 20:10:25 -0800

    修复提交问题的入口Bug

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@
         documentTitle:"DataStorm By Romanysoft",
         googleUA:"UA-43238484-5",
         gitHome:"https://github.com/Romanysoft/DataStorm",
+        reportIssueUrl:"https://github.com/Romanysoft/DataStorm/issues",
         romanysoftHome:"http://www.romanysoft.com",
         copyright:"Copyright © 2016 Romanysoft LAB."
     }
