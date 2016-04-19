bd0d322c33030ad19c9fb3305653b4c85f3da1fe
iansun <lauer3912@gmail.com>
2016-04-19 20:40:55 +0800

    Update

--- js/config.js
+++ js/config.js
@@ -5,11 +5,11 @@
         documentTitle:"DataStorm By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
         googleUA:"UA-43238484-5",
-        gitHome:"https://github.com/Romanysoft/DataStorm",
-        reportIssueUrl:"https://github.com/Romanysoft/DataStorm/issues",
-        changeLogUrl:"https://github.com/Romanysoft/DataStorm/wiki/Changelog",
-        wikiUrl:"https://github.com/Romanysoft/DataStorm/wiki",
-        romanysoftHome:"http://www.romanysoft.com",
+        gitHome:"//github.com/Romanysoft/DataStorm",
+        reportIssueUrl:"//github.com/Romanysoft/DataStorm/issues",
+        changeLogUrl:"//github.com/Romanysoft/DataStorm/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/DataStorm/wiki",
+        romanysoftHome:"//www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
 })();
