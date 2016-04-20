434ac134381b038e95048c052c0f9ac2fad3083b
iansun <lauer3912@gmail.com>
2016-04-20 13:55:01 +0800

    使用新的google跟踪号

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
         appID:"com.romanysoft.app.macos.DataStorm",
         documentTitle:"DataStorm By Romanysoft",
         supportPlatforms:["Mac","Windows","Linux"],
-        googleUA:"UA-43238484-5",
+        googleUA:"UA-76676167-1",
         gitHome:"//github.com/Romanysoft/DataStorm",
         reportIssueUrl:"//github.com/Romanysoft/DataStorm/issues",
         changeLogUrl:"//github.com/Romanysoft/DataStorm/wiki/Changelog",
