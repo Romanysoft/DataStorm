5c42af9a95b5203ec458bb38982c151c9b0bbc4a
iansun <lauer3912@gmail.com>
2017-02-24 09:01:49 +0800

    update

--- js/config.js
+++ js/config.js
@@ -9,7 +9,12 @@
         reportIssueUrl:"//github.com/Romanysoft/DataStorm/issues",
         changeLogUrl:"//github.com/Romanysoft/DataStorm/wiki/Changelog",
         wikiUrl:"//github.com/Romanysoft/DataStorm/wiki",
+        jumpLocation:"https://www.romanysoft.net/datastorm/",
         romanysoftHome:"//www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
+    };
+    
+    if(window.RTYConfig.jumpLocation){
+        window.location = window.RTYConfig.jumpLocation;
     }
 })();
